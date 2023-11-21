ba95ed0d2faf2211ed810da714c0849a63e18e4e
Eduardo <eduher.rodp@gmail.com>
2023-11-21 02:29:56 -0600

    app completed

--- src/routes/mqttRoutes.js
+++ src/routes/mqttRoutes.js
@@ -1,4 +1,3 @@
-// mqttRoutes.js
 import express from 'express';
 import MQTTClient from '../services/MQTTClient.js';
 import { loadConfig } from '../app.js';
@@ -8,18 +7,16 @@ const router = express.Router();
 router.post('/send-message', async (req, res) => {
     try {
         const mqttConfig = await loadConfig();
+
         const { host, port, clientId, username, password, mountpoint } = mqttConfig.mqtt;
 
         const mqttClient = new MQTTClient({ host, port, clientId, username, password, mountpoint });
 
-        // Verificar si el cliente está conectado antes de intentar publicar
-        if (mqttClient.getClient().connected) {
-            const { message } = req.body;
-            mqttClient.getClient().publish('/test/topic', message);
-            res.json({ status: 'Message sent to MQTT broker', message });
-        } else {
-            res.status(500).json({ error: 'MQTT client not connected' });
-        }
+        const { message } = req.body;
+
+        // Publicar el mensaje en el topic MQTT
+        mqttClient.getClient().publish('/test/topic', message);
+        res.json({ status: 'Message sent to MQTT broker', message });
     } catch (error) {
         console.error('Error in MQTT route: ', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
--- src/services/MQTTClient.js
+++ src/services/MQTTClient.js
@@ -2,54 +2,32 @@ import * as MQTT_L from 'mqtt';
 
 class MQTTClient {
     constructor({ host, port, clientId, username, password, mountpoint = '/mqtt' }) {
-        // console.log('Received MQTT config:', { host, port, clientId, username, password, mountpoint });
 
         if (!host || !port) {
             throw new Error('Invalid MQTT configuration: host and port are required.');
         }
 
-        this.mqttUrl = `ws://${host}:${port}${mountpoint}`;
-        this.connect();
-    }
+        const mqttUrl = `ws://${host}:${port}${mountpoint}`;
 
-    connect() {
-        this.client = MQTT_L.connect(this.mqttUrl, {
-            clientId: this.clientId,
-            username: this.username,
-            password: this.password,
+        this.client = MQTT_L.connect(mqttUrl, {
+            clientId,
+            username,
+            password,
         });
 
-        this.setupEventListeners();
-    }
-
-    setupEventListeners() {
         this.client.on('connect', () => {
-            console.log('Connected to MQTT broker: ' + this.host);
+            console.log('Connected to MQTT broker: ' + host);
             this.client.subscribe('/test/topic', (err) => {
                 if (!err) {
                     console.log('Subscribed to /test/topic');
                 }
             });
-            this.client.publish('/test/topic', `Client ${this.clientId} has been connected`);
+            this.client.publish('/test/topic', `Client ${clientId} has been connected`);
         });
 
         this.client.on('error', (err) => {
             console.error('MQTT connection error', err.message);
-            this.scheduleReconnect();
         });
-
-        this.client.on('close', () => {
-            console.log('Connection to MQTT broker closed');
-            this.scheduleReconnect();
-        });
-    }
-
-    scheduleReconnect() {
-        // Intentar la reconexión después de un intervalo de tiempo
-        setTimeout(() => {
-            console.log('Attempting to reconnect to MQTT broker...');
-            this.connect();
-        }, 5000);
     }
 
     getClient() {
