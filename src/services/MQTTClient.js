b22a2a83c6469504f75e23687f8cc5ce9c70f3a0
Eduardo <eduher.rodp@gmail.com>
2023-11-20 23:44:43 -0600

    project structure - services/MQTTClient.js

---
+++ src/services/MQTTClient.js
@@ -0,0 +1,29 @@
+import mqtt, * as MQTT_L from 'mqtt';
+
+class MQTTClient {
+    constructor(mqttConfig) {
+        this.client = MQTT_L.connect(`ws://${mqttConfig.host}:${mqttConfig.port}/${mqttConfig.mountpoint}`, {
+            clientId: mqttConfig.clientId,
+            username: mqttConfig.username,
+            password: mqttConfig.password,
+        });
+
+        this.client.on('connect', () => {
+            console.log('Connected to MQTT broker: ' + mqttConfig.host);
+            this.client.subscribe('/test/topic', (err) => {
+                if (!err) console.log('Subscribed to /test/topic');
+            });
+            this.client.publish('/test/topic', `Client ${mqttConfig.clientId} has been connected`);
+        });
+        this.client.on('error', (err) => {
+            console.log('MQTT connection error', err.message);
+        });
+    }
+
+    getClient() {
+        return this.client;
+    }
+}
+
+const mqttClientInstance = new MQTTClient(/* TODO: Pasar la configuración */);
+export default mqttClientInstance;
