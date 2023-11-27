533615f889f251b3d0ed047902f4db690a205373
Eduardo <eduher.rodp@gmail.com>
2023-11-26 23:41:31 -0600

    send-to-mqtt-api - endpoint

--- web_app/js/LEDController.js
+++ web_app/js/LEDController.js
@@ -203,7 +203,7 @@ class LEDController {
         const formattedBinaryStringFinal = this.generateFormattedBinaryString();
         const message = { message: formattedBinaryStringFinal };
 
-        fetch('http://app.smarttransit.online:3000/send-message', {
+        fetch('http://127.0.0.1:3000/send-message', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
